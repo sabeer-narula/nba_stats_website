239f56823f9144f0c6515ebe7f72800171802fa9
Sabeer Narula <sn23@bu.edu>
2024-06-18 13:40:57 -0700

    adjusting weightings and changing criteria for most underpaid players

--- nba_stats_website/website/overpaid_score.py
+++ nba_stats_website/website/overpaid_score.py
@@ -18,16 +18,16 @@ def calculate_gp_weight(games_played):
 
 def calculate_overpaid_metric(player_data):
     POSITION_WEIGHTS = {
-        'C': {'BLK': 0.09, 'PTS': 0.18, 'AST': 0.04, 'FG_PCT': 0.04, 'FT_PCT': 0.03, 'FG3_PCT': 0.02, 'OREB': 0.05, 'DREB': 0.05},
-        'PG': {'BLK': 0.01, 'PTS': 0.23, 'AST': 0.06, 'FG_PCT': 0.06, 'FT_PCT': 0.05, 'FG3_PCT': 0.05, 'OREB': 0.02, 'DREB': 0.02},
-        'SG': {'BLK': 0.01, 'PTS': 0.24, 'AST': 0.04, 'FG_PCT': 0.06, 'FT_PCT': 0.05, 'FG3_PCT': 0.05, 'OREB': 0.03, 'DREB': 0.02},
-        'SF': {'BLK': 0.04, 'PTS': 0.21, 'AST': 0.04, 'FG_PCT': 0.05, 'FT_PCT': 0.04, 'FG3_PCT': 0.04, 'OREB': 0.04, 'DREB': 0.04},
-        'PF': {'BLK': 0.05, 'PTS': 0.21, 'AST': 0.04, 'FG_PCT': 0.04, 'FT_PCT': 0.04, 'FG3_PCT': 0.02, 'OREB': 0.05, 'DREB': 0.05}
+        'C': {'BLK': 0.10, 'PTS': 0.23, 'AST': 0.04, 'FG_PCT': 0.04, 'FT_PCT': 0.03, 'FG3_PCT': 0.02, 'OREB': 0.05, 'DREB': 0.05},
+        'PG': {'BLK': 0.01, 'PTS': 0.28, 'AST': 0.06, 'FG_PCT': 0.06, 'FT_PCT': 0.05, 'FG3_PCT': 0.05, 'OREB': 0.02, 'DREB': 0.02},
+        'SG': {'BLK': 0.01, 'PTS': 0.29, 'AST': 0.04, 'FG_PCT': 0.06, 'FT_PCT': 0.05, 'FG3_PCT': 0.05, 'OREB': 0.03, 'DREB': 0.02},
+        'SF': {'BLK': 0.04, 'PTS': 0.26, 'AST': 0.04, 'FG_PCT': 0.05, 'FT_PCT': 0.04, 'FG3_PCT': 0.04, 'OREB': 0.04, 'DREB': 0.04},
+        'PF': {'BLK': 0.05, 'PTS': 0.26, 'AST': 0.04, 'FG_PCT': 0.04, 'FT_PCT': 0.04, 'FG3_PCT': 0.02, 'OREB': 0.05, 'DREB': 0.05}
     }
 
     DEFAULT_WEIGHTS = {
         'BLK': 0.03,
-        'PTS': 0.21,
+        'PTS': 0.25,
         'AST': 0.04,
         'FG_PCT': 0.07,
         'FT_PCT': 0.04,
@@ -42,14 +42,14 @@ def calculate_overpaid_metric(player_data):
     gp_weight = calculate_gp_weight(player_data['GP'])
 
     value_score = (
-        (gp_weight * player_data['GP']) + (0.05 * player_data['MIN']) +
+        (gp_weight * player_data['GP']) + (0.08 * player_data['MIN']) +
         (weights['FG_PCT'] * player_data['FG_PCT']) + (weights['FG3_PCT'] * player_data['FG3_PCT']) +
         (weights['FT_PCT'] * player_data['FT_PCT']) + (weights['OREB'] * player_data['OREB']) +
         (weights['DREB'] * player_data['DREB']) + (weights['AST'] * player_data['AST']) +
         (-0.04 * player_data['TOV']) + (0.02 * player_data['STL']) + (weights['BLK'] * player_data['BLK']) +
         (weights['PTS'] * player_data['PTS']) + (0.8 * player_data['PLUS_MINUS']) +
-        (0.04 * player_data['WS/48']) + (0.05 * player_data['BPM']) + (0.07 * player_data['VORP']) +
-        (0.20 * player_data['PER'])
+        (0.06 * player_data['WS/48']) + (0.05 * player_data['BPM']) + (0.08 * player_data['VORP']) +
+        (0.26 * player_data['PER'])
     )
 
     salary = player_data['SALARY']
@@ -69,7 +69,7 @@ def main():
                     if row[key] != '':
                         row[key] = float(row[key])
                     else:
-                        row[key] = 0.0  # or assign a default value
+                        row[key] = 0.0
 
             # Check if the player has a valid salary
             if row['SALARY'] > 0:
@@ -77,6 +77,8 @@ def main():
                 player_info = {
                     'name': row['PLAYER_NAME'],
                     'salary': row['SALARY'],
+                    'GP': row['GP'],
+                    'minutes': row['MIN'],
                     'overpaid_metric': overpaid_metric
                 }
                 players.append(player_info)
@@ -86,7 +88,7 @@ def main():
     overpaid_players = [player for player in players if player['salary'] >= 10000000]
     overpaid_players.sort(key=lambda x: x['overpaid_metric'], reverse=True)
     print("Top 20 Most Overpaid Players (Salary >= $10 million):")
-    for i, player in enumerate(overpaid_players[:20], start=1):
+    for i, player in enumerate(overpaid_players[:40], start=1):
         print(f"{i}. {player['name']}: ${player['salary']}: Overpaid Metric: {player['overpaid_metric']:.2f}")
 
     print("\nOverpaid Metrics for the 20 Highest Paid Players:")
@@ -95,9 +97,9 @@ def main():
         print(f"{i}. {player['name']}: ${player['salary']}: Overpaid Metric: {player['overpaid_metric']:.2f}")
 
     print("\nTop 20 Most Underpaid Players:")
-    valid_players = [player for player in players if player['salary'] > 10000000]
+    valid_players = [player for player in players if (player['salary'] >= 10000000 and (player['GP']*player['minutes'] > 0.3))]
     valid_players.sort(key=lambda x: x['overpaid_metric'])
-    for i, player in enumerate(valid_players[:20], start=1):
+    for i, player in enumerate(valid_players[:25], start=1):
         print(f"{i}. {player['name']}: ${player['salary']}: Underpaid Metric: {player['overpaid_metric']:.2f}")
 
 if __name__ == '__main__':
